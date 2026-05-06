6ba419aca0b9b12ce40f8a179d1b51e960d2a7c7
Alexandr Mertl <amertl@redhat.com>
2026-05-06 17:40:05 +0200

    Included all html files in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
     host: true,
     port: 5173
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        'main': resolve(__dirname, 'index.html'),
+        'gallery': resolve(__dirname, 'gallery.html'),
+        'instructions': resolve(__dirname, 'instructions.html'),
+        'how-to-get-here': resolve(__dirname, 'how-to-get-here.html'),
+        'tips-and-trips': resolve(__dirname, 'tips-and-trips.html')
+      }
+    }
   }
 })
