40c9021065600c3eaa09c4d485d583e51df395b5
Alexandr Mertl <amertl@redhat.com>
2026-05-21 11:26:17 +0200

    Changed some styling and formatting of code, improved readability

--- src/navigation.js
+++ src/navigation.js
@@ -5,7 +5,7 @@ export function renderNavigation() {
       <li><a href="/index.html">Home</a></li>
       <li><a href="/how-to-get-here.html">How to get here</a></li>
       <li><a href="/instructions.html">Instructions</a></li>
-      <li><a href="/tips-and-trips.html">Tips & Trips</a></li>
+      <li><a href="/trips.html">Trips</a></li>
       <li><a href="/gallery.html">Gallery</a></li>
     </ul>
   `
